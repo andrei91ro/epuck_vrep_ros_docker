46b24996cc403259ba202250b826b9bd7f143b02
Andrei Florea <andrei91ro@gmail.com>
2017-12-20 01:09:17 +0200

    removed catkin_ws volume

--- start.bash
+++ start.bash
@@ -4,7 +4,6 @@ docker run -it \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="/home/andrei/catkin_ws/src:/root/catkin_ws/src:rw" \
     --device=/dev/dri:/dev/dri \
     --name epuck_vrep_ros_indigo \
     --hostname epuck_vrep_ros_indigo \
